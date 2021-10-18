1be31503a3995732d54c7085d802de1e27746f56
rizwanullah313 <ru323091@gmail.com>
2021-10-18 04:53:01 +0000

    Show Selected Itmes With Details

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -7,14 +7,38 @@ class Menu extends Component {
         super(props);
 
         this.state = {
+            selectedDish: null
         };
 
+    }
+
+    onDishSelect(dish){
+        this.setState({selectedDish: dish});
+    }
+    renderDish(dish){
+        if(dish != null){
+            return(
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                    <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+
+        }
+        else{
+            return(
+                <div></div>
+            );
+        }
     }
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                   <Card tag="li" className="lsi">
+                   <Card onClick={()=>{this.onDishSelect(dish)}}>
                            <CardImg width="100%" src={dish.image} alt={dish.name}/>
                            <CardImgOverlay body className="ml-5">
                                <CardTitle>{dish.name}</CardTitle>
@@ -27,6 +51,12 @@ class Menu extends Component {
             <div className="container">
                 <div className="row">
                         {menu}
+                </div>
+                <hr/>
+                <h4>Selected Itmes Details</h4>
+                <hr/>
+                <div className="row">
+                 {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
         );
